24022c48bbce7dd4bdd4ff7c3b2c46798d3b9f39
Tuna8 <tina.zwittnig@gmail.com>
2016-03-11 17:13:20 +0100

    Update vauvau.py

--- vauvau.py
+++ vauvau.py
@@ -1,45 +1,42 @@
 import sys
-def psi(psi,postar):
+def psi(psi, postar):
     psi.strip()
-    laja=psi.split()
-    laja1=int(laja[0])
-    spi1=int(laja[1])
-    ali=postar%(laja1+spi1)
-    if 0<ali<=laja1:
+    laja = psi.split()
+    laja1 = int(laja[0])
+    spi1 = int(laja[1])
+    ali = postar % (laja1+spi1)
+    if 0 < ali <= laja1:
         return True
     else:
 
         return False
 vnesi=""
 n=2
-while n>0:
-    vrstica=input()+"\n"
-    vnesi+=vrstica
-    n-=1
-print(vnesi)
+while n > 0:
+    vrstica = input() + "\n"
+    vnesi += vrstica
+    n -= 1
 vnesi.split("\n")
-print(vnesi)
-lajajo=vnesi[:8]
-postar=vnesi[8:]
-postar=postar.split()
-pes1=lajajo[:3]
-pes2=lajajo[3:]
-sez=[]
-print(postar)
-n=0
-while n<=2:
+lajajo = vnesi[:8]
+postar = vnesi[8:]
+postar = postar.split()
+pes1 = lajajo[:3]
+pes2 = lajajo[3:]
+sez = []
+n = 0
+while n <= 2:
     lajajo=[]
-    if psi(pes1,int(postar[n])):
+    if psi(pes1, int(postar[n])):
         lajajo.append(True)
     if psi(pes2,int(postar[n])):
         lajajo.append(True)
     sez.append(lajajo)
-    n+=1
+    n += 1
 
 for dolzina in sez:
-    if len(dolzina)==0:
+    if len(dolzina) == 0:
         print("none")
-    elif len(dolzina)==1:
+    elif len(dolzina) == 1:
         print("one")
     else:
         print("both")
