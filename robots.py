c91e1b65d596ea4b3a5bd2f5f43feb30959eb9ca
Tina Zwittnig <tina.zwittnig@gmail.com>
2016-03-12 11:12:15 +0100

    Dokončno rešila nalogo

--- robots.py
+++ robots.py
@@ -1,5 +1,6 @@
-besedilo=""
-while True
+napisi=""
+while True:
+    besedilo=""
     vnesi=input()
     if vnesi=="0 0":
         break
@@ -12,8 +13,34 @@ while True
     ry=0
     n=int(input())
     while n>0:
+        n-=1
         hodi = input()
         hodi = hodi.split()
         smer = hodi[0]
         kol = int(hodi[1])
-        if rx>vx
+        if smer=="u":
+            my+=kol
+            ry+=kol
+        elif smer=="d":
+            my-=kol
+            ry-=kol
+        elif smer=="l":
+            mx-=kol
+            rx-=kol
+        elif smer=="r":
+            mx+=kol
+            rx+=kol
+        if rx>vx:
+            rx=vx
+        elif rx<0:
+            rx=0
+        if ry>vy:
+            ry=vy
+        elif ry<0:
+            ry=0
+    besedilo+="Robot thinks "+ str(mx)+ " "+ str(my)+"\n"
+    besedilo+= "Actually at " +str(rx)+" "+ str(ry)+"\n"+"\n"
+    napisi+=besedilo
+print(napisi)
+
+
